3d0aa723a40deded36fa2a349a8f20ed562a4e75
Ezra J <ezra.jesalva@gmail.com>
2018-05-13 11:35:11 -0400

    Set up connection with api

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ $(document).ready(function(){
   let users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp",
    "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
 
-  let url= 'https://wind-bow.gomix.me/twitch-api';
+  let url = 'https://wind-bow.glitch.me/twitch-api/users/ESL_SC2';
 
-  for( let i =0; i < users.length;i++){
-    console.log(users[i]);
-  }
-  // $.get( )
+   $.get(url,function(data){
+     console.log(data);
+   });
 
 
 
