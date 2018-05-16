751d05ab429ed0c582c23f92a68ce207357e5ecc
Ezra Jesalva <ezra.jesalva15@gmail.com>
2018-05-16 18:31:42 -0400

    Fix focus outline on input to imitate radio buttons. Put in status details if online. Fix responsiveness for status details and buttons. Change styling depending on status.

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
+
+
   let users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp",
    "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas","ninja"];
 
   let url = 'https://wind-bow.glitch.me/twitch-api/users/';
   let urlStream = 'https://wind-bow.glitch.me/twitch-api/streams/'
+
+
 
   //I want to be able to loop through all of these...
   for(let i=0;i<users.length;i++){
@@ -16,16 +20,22 @@ $(document).ready(function(){
 
       //get online status
       $.get(urlStream + users[i],function(data){
+
         let status = data["stream"];
 
+        let  statusDetails  = () =>{ //details is user is online
+          onlineStatus.innerHTML = status["channel"]["status"];
+          onlineStatus.style.whiteSpace = 'normal';
 
+            // onlineStatus.innerHTMl = status["channel"]["status"];
+        };
 
         //create doc elements
         let section = document.createElement("section");
         let img = document.createElement("img");
-        let user = document.createElement("p");
+        let user = document.createElement("a");
 
-        let onlineStatus = document.createElement("p");
+        let onlineStatus = document.createElement("P");
 
         //give attributes
         section.className = "section";
@@ -33,14 +43,26 @@ $(document).ready(function(){
         img.src = logo;
         user.className = "user";
         user.innerHTML = name;
+        user.href = 'https://www.twitch.tv/' + name;
+        user.target = "_blank";
         onlineStatus.className = "status";
 
         onlineStatus.innerHTML = status === null?"offline":"online";
-        if(onlineStatus.innerHTML==="online"){ onlineStatus.style.color = "green"; section.classList.add("online")} else{
+        if(onlineStatus.innerHTML==="online"){
+          statusDetails();
+
+          $("this").parent().css("color","green");
+          section.style.background = "green";
+          section.classList.add("online") } else{
+          onlineStatus.style.color = "red";
           section.classList.add("offline");
+
          }
 
-        console.log(onlineStatus.classList);
+
+
+
+
         //append
         section.append(img);
         section.append(user)
@@ -48,6 +70,8 @@ $(document).ready(function(){
 
         //put into document
         $(".body").append(section);
+
+
 
       })
 
@@ -57,6 +81,8 @@ $(document).ready(function(){
 
 
   }
+
+
 
 
   let filterOnline = ()=>{
@@ -80,6 +106,8 @@ $(document).ready(function(){
   };
 
 
+
+
   $("input[name='offline']").click(function(){
     filterOffline();
   });
@@ -93,6 +121,21 @@ $(document).ready(function(){
       showAll();
     });
 
-  
+  $("input[type^=but]").focus(function(e){
+    e.preventDefault();
+    console.log('this',$(this));
+    $("input").css("outline","none");
+    this.style.outlineWidth = '3px';
+    this.style.outlineStyle = 'solid';
+    this.style.outlineRadius = '2px;'
+    this.style.outlineColor = 'blue';
+  })
+
+
+
+
+
+
+
 
 });
